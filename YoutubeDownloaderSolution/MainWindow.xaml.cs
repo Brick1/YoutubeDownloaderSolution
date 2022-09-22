76972a8112e10ec9d895c8645220f59898b0874f
Václav Komárek <v.komarek@outlook.com>
2022-09-22 19:32:20 +0200

    Added switching between main window and loading window

--- YoutubeDownloaderSolution/MainWindow.xaml.cs
+++ YoutubeDownloaderSolution/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,20 @@ namespace YoutubeDownloaderSolution
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Visibility = Visibility.Hidden;
+            LoadingWindow loading = new LoadingWindow();
+            loading.Show();
+            //await Task.Delay(TimeSpan.FromSeconds(3));
+            Thread.Sleep(2000);
+            loading.Hide();
+            this.Visibility = Visibility.Visible;
+
+        }
+
+        private void ClsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
